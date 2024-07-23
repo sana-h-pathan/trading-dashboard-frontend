61fd7a1378799ae491162cf769c863f0c5aaa26b
Sana Pathan <sanapathan28@gmail.com>
2024-07-22 22:29:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import User from './components/User';
+import Watchlist from './components/Watchlist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trading Dashboard</h1>
       </header>
+      <User />
+      <Watchlist />
     </div>
   );
 }
---
+++ src/components/Watchlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Watchlist = () => {
+  return (
+    <div>
+      <h2>Watchlist Component</h2>
+    </div>
+  );
+};
+
+export default Watchlist;
